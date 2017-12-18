a9d38f40dc073aadda1238c1b61601fe711a9f90
abhi2519 <34072336+abhi2519@users.noreply.github.com>
2017-12-18 14:12:15 +0530

    compulsory argument written

--- 1.sh
+++ 1.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+LOC=$1
+EXT=$1
+NUMFILE=0
+NUMDIR=0
+
+for item in $loc/*$EXT
+do
+if [ -f "$item" ]
+  then
+    NUMFILE=$[NUMFILE+1]
+  elif [ -d "$item" ]
+    then
+     NUMDIR=$[NUMDIR+1]
+     
+fi
+done
+
+echo The number of files are $NUMFILE
